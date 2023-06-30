646eb56dea281d217cea65e35a7a56408df753bf
Penelope Drago <prd@drago.com.au>
2023-06-30 10:17:54 +1000

    add countdown timer with useEffect hook

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,22 @@
-import React, { useState } from "react";
+import React, {useEffect, useState} from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
   // add useEffect code
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      const newTimeRemaining = timeRemaining - 1;
+      setTimeRemaining(newTimeRemaining);
+      if (newTimeRemaining === 0) {
+        setTimeRemaining(10);
+        onAnswered(false);
+      }
+    }, 1000);
+    return function cleanup() {
+      clearTimeout(timer); 
+    }
+  }, [timeRemaining])
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
